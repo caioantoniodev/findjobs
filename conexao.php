ae833ec5d1ef3c82aeceb75f6d5f71205e9dc073
Caio Cichetti <caio_cichetti@outlook.com>
2020-07-05 12:41:15 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,5 +1,8 @@
 <?php
-  // Criando a conexão com o servidor do MySQL
+  // Criando a conexão com o servidor do MySQL Windows
   $conexao = mysqli_connect('localhost', 'root', '', 'findjobs') 
     or die ('Não foi possível acessar o BD');
+  // Criando a conexão com o servidor do MySQL Linux Docker
+  // $conexao = mysqli_connect('172.17.0.2', 'root', '', 'findjobs') 
+  //   or die ('Não foi possível acessar o BD');
 ?>
