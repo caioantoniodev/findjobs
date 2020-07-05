9ef472b0da408574d2a6d9d5bd9e978f793952f9
Caio Cichetti <caio_cichetti@outlook.com>
2020-07-05 13:42:18 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,7 @@
-function verificaDelete(Id) {
-    let id = Id;
-
-    if (confirm("Deseja excluir este projeto? ")) {
-      location.href=`delete.php?id=${id}`;
-    }
+// Recebe o id do projeto que recebeu o onclick() e exibe um "confirm" 
+function verificaDelete(Id) { 
+  let id = Id; 
+  if (confirm("Deseja excluir este projeto? ")) { 
+    location.href=`delete.php?id=${id}`; 
+  } 
 }
-
-function maskcpf(){
-  var cpf = document.getElementById('cpf')
-
-  if(cpf.value.length == 3 || cpf.value.length == 7){
-      cpf.value += "."
-  }
-  
-  if(cpf.value.length == 11){
-    cpf.value += "-"
-  }
-}
-
