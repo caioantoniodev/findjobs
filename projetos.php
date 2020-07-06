d5e1a41aa7cdea0f35102a695e4f1030f70010e4
Caio Cichetti <caio_cichetti@outlook.com>
2020-07-06 16:21:57 -0300

    area projetos finalizada

--- projetos.php
+++ projetos.php
@@ -1,31 +1,36 @@
+<?php
+    session_start();
+    include('conexao.php');
+
+    // verifico se está logado, assim impedindo acessar direto na url
+    if (!isset($_SESSION['logado'])) {
+        header('Location: index.php');
+    }    
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="styles/projetos.css" rel="stylesheet" type="text/css">
-    <link type="text/css" href="styles/loading.css" rel="stylesheet">
-    <link type="text/css" href="styles/topo.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
     <link rel="shortcut icon" href="images/LogoBranca32.png"/>
     <title>Find Jobs</title>
 </head>
 <body>
-    <div id="content">
-        <div id="spinner"></div>
-    </div>
-    <header class="topo" id="conteudo">
+    <header class="topo">
         <img src="images/LogoAzul.png">
         <nav class="menu">
             <div class="icones">
-                <h1 class="twitter"><a href="https://twitter.com/FindJobsTCC" target="_blank"><i class="fab fa-twitter-square"></i></a></h1>
-                <h1 class="facebook"><a href="https://www.facebook.com/Find-Jobs-111396177288447/" target="_blank"><i class="fab fa-facebook-square"></i></a></h1>
+                <h1 class="twitter"><a href="https://twittercom/" target="_blank"><i class="fab fa-twitter-square"></i></a></h1>
+                <h1 class="facebook"><a href="https://pt-br.facebook.com/" target="_blank"><i class="fab fa-facebook-square"></i></a></h1>
             </div>
             <ul>
                 <li><a href="index.php">Inicio</a></li>
-                <li><a href="aulas.php">Aulas</a></li>
+                <li><a href="">Aulas</a></li>
                 <li><a href="projetos.php">Projetos</a></li>
-                <li><a href="contato.php">Projetos</a></li>
+                <li><a href="contato.php">Contato</a></li>
                 <li><a href="sair.php">Sair</a></li>
             </ul>
         </nav>
@@ -33,67 +38,63 @@
             <h1>Bem vindo a area de projetos!</h1>
             <p>Aqui você encontra diversas opções de projetos para ingressar :)</p>
             <div class="button">
-                <a href="criar.html" class="btn btn-one">Criar projeto</a>
+                <a href="" class="btn btn-one">Criar projeto</a>
             </div>
         </div>
     </header>
     <section>
         <div class="projetos">
-            <div class="projeto">
-                <img src="images/projeto.png" alt="Avatar">
-                <div class="container-p">
-                    <div class="icones-p">
-                        <abbr title="Repositório"><a class="git" href="https://github.com/" target="_blank"><i class="fab fa-github-square"></i></a></abbr>
+            <?php
+                // pegando nome e a profissao do freela
+                $cpf = $_SESSION['cpf'];
+
+                // pegando nome e a profissao do freela
+                $info_freela = mysqli_query($conexao, "SELECT usuarios.nome, usuarios.profissao FROM  usuarios WHERE cpf = $cpf;");
+                $dados = mysqli_fetch_assoc($info_freela);
+                $nome_freela = $dados['nome'];
+                $profissao_freela = $dados['profissao'];
+
+                // listando projetos  e seus respectivos donos
+                $consulta = "SELECT * FROM usuarios, projetos WHERE usuarios.cpf = projetos.cliente_cpf AND usuarios.cpf != $cpf;";
+
+                // recebo o resutado da querie na variavel $resultado
+                $resultado = mysqli_query($conexao, $consulta);
+                
+                // utilizo o while para percorrer cada card de do html e add as infos do bd
+                while($dados = mysqli_fetch_assoc($resultado)) {
+            ?>
+                <div class="projeto">
+                    <img src="<?=$dados['imgurl']?>" alt="Avatar">
+                    <div class="container-p">
+                        <?php 
+                            // guardando alguns valores que serão utilizados
+                            // pegando informações referentes ao cliente
+                            $nome_cliente = $dados['nome'];  
+                            $email_cliente = $dados['email'];
+                            $titulo_projeto = $dados['titulo'];
+                            $id_projeto = $dados['idprojetos'];
+
+                            // Verifico se o projeto está em aberto
+                            if ($dados['cpffreela'] == NULL) {
+                        ?>
+                            <h4><?=$dados['nome']?></h4>
+                            <p><?=$dados['linguagem']?></p>
+                            <p><?=$dados['salario']?></p>
+                            <p><?=$dados['titulo']?></p>
+                            <br><p><p><?=$dados['descricao']?></p></p>
+                            
+                            <!--Botao que redireciona o freela para o email do cliente-->
+                            <div class="button-p">
+                                <a onclick="enviarEmail('<?=$email_cliente?>', '<?=$nome_cliente?>', '<?=$titulo_projeto?>', '<?=$nome_freela?>')" class="btn-p btn-one">Tenho Interesse</a>
+                            </div>
+                        <?php } ?>
                     </div>
-                    <h4>Dono</h4>
-                    <p>Linguagem</p>
-                    <p>Valor</p>
-                    <br><p>Preciso de uma calculadora de imc</p>
-                    <div class="button-p">
-                        <a href="" class="btn-p btn-one">Tenho Interesse</a>
-                    </div>
-                </div>
-            </div>
-            <div class="projeto">
-                <img src="images/projeto2.png" alt="Avatar">
-                <div class="container-p">
-                    <div class="icones-p">
-                        <abbr title="Repositório"><a class="git" href="https://github.com/" target="_blank"><i class="fab fa-github-square"></i></a></abbr>
-                        <abbr title="Editar Projeto"><a class="edit" href=""><i class="fas fa-pen-square"></i></a></abbr>
-                        <abbr title="Excluir Projeto"><a class="del" href="" onclick="verificaDelete()"><i class="fas fa-minus-square"></i></i></a></abbr>
-                    </div>
-                    <h4>Dono</h4>
-                    <p>Linguagem</p>
-                    <p>Valor</p>
-                    <br><p>Preciso de uma calculadora de imc</p>
-                    <div class="button-p">
-                        <a href="" class="btn-p btn-one">Tenho Interesse</a>
-                    </div>
-                </div>
-            </div>
-            <div class="projeto">
-                <img src="images/projeto3.png" alt="Avatar">
-                <div class="container-p">
-                    <div class="icones-p">
-                        <abbr title="Sem Repositório"><a class="s-git" href="" onclick="alert('Ops... esse projeto não possui Repositório')"><i class="fab fa-github-square"></i></a></abbr>
-                    </div>
-                    <h4>Dono</h4>
-                    <p>Linguagem</p>
-                    <p>Valor</p>
-                    <br><p>Preciso de uma calculadora de imc</p>
-                    <div class="button-p">
-                        <a href="" class="btn-p btn-one">Tenho Interesse</a>
-                    </div>
-                </div>
-            </div>
+                </div> 
+            <?php } ?>
         </div>
     </section>
 
-    <button onclick="backToTop()" id="btnTop"><i class="fas fa-arrow-up"></i></button>
-
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js"></script>
-    <script type="text/JavaScript" src="scripts/loading.js"></script>
-    <script type="text/JavaScript" src="scripts/topo.js"></script>
-    <script type="text/JavaScript" src="scripts/script.js"></script>
+    <script src="scripts/script.js"></script>
 </body>
 </html>
--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,14 @@ function verificaDelete(Id) {
     location.href=`delete.php?id=${id}`; 
   } 
 }
+
+function enviarEmail(email, nome, projeto, nomeFreela) {
+  let Email = email;
+  let Nome = nome;
+  let Projeto = projeto;
+  let nomefreela = nomeFreela;
+
+  if(confirm(`Você deseja enviar um e-mail para ${Nome}`)) {
+    location.href = `mailto:${Email}?subject=Interesse no projeto ${Projeto}&body=Olá ${Nome}! Meu nome é ${nomefreela} Sou desenvolvedor back-end e vi seu projeto ${Projeto} na plataforma Find Jobs e eu tenho interesse em ajudar.`
+  }
+} 
