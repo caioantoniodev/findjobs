03e435f5a85b5f641fdacf2d87fb8fe46798d88e
Caio Cichetti <caio_cichetti@outlook.com>
2020-07-08 11:57:51 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -6,13 +6,14 @@ function verificaDelete(Id) {
   } 
 }
 
-function enviarEmail(email, nome, projeto, nomeFreela) {
+function enviarEmail(email, nome, projeto, nomeFreela, profissaoF) {
   let Email = email;
   let Nome = nome;
   let Projeto = projeto;
   let nomefreela = nomeFreela;
+  let profissaoFreela = profissaoF;
 
   if(confirm(`Você deseja enviar um e-mail para ${Nome}`)) {
-    location.href = `mailto:${Email}?subject=Interesse no projeto ${Projeto}&body=Olá ${Nome}! Meu nome é ${nomefreela} Sou desenvolvedor back-end e vi seu projeto ${Projeto} na plataforma Find Jobs e eu tenho interesse em ajudar.`
+    location.href = `mailto:${Email}?subject=Interesse%no%projeto%${Projeto}&body=Olá%${Nome}!%Meu%nome%é%${nomefreela}%Sou%desenvolvedor(a)%${profissaoFreela}%e%vi%seu%projeto%${Projeto}%na%plataforma%Find%Jobs%e%eu%tenho%interesse%em%ajudar.`
   }
 } 
