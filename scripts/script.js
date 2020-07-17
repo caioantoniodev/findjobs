560a19c2186e4d718345022c8e6a34f0f348be52
Caio Cichetti <caio_cichetti@outlook.com>
2020-07-17 12:02:09 -0300

    erro arrumado

--- scripts/script.js
+++ scripts/script.js
@@ -10,10 +10,10 @@ function enviarEmail(email, nome, projeto, nomeFreela, profFreela) {
   let Email = email;
   let Nome = nome;
   let Projeto = projeto;
-  let nomefreela = nomeFreela;
-  let profFreela = profFreela;
+  let Nomefreelancer = nomeFreela;
+  let ProfFreelancer = profFreela;
 
   if(confirm(`Você deseja enviar um e-mail para ${Nome}`)) {
-    location.href = `mailto:${Email}?subject=Interesse no projeto ${Projeto}&body=Olá ${Nome}! Meu nome é ${nomefreela} Sou desenvolvedor back-end e vi seu projeto ${Projeto} na plataforma Find Jobs e eu tenho interesse em ajudar.`
+    location.href = `mailto:${Email}?subject=Interesse no projeto ${Projeto}&body=Olá ${Nome}! Meu nome é ${Nomefreelancer} Sou ${ProfFreelancer} e vi seu projeto ${Projeto} na plataforma Find Jobs e eu tenho interesse em ajudar.`
   }
 } 
