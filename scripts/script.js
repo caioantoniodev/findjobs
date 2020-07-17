7d9373e92de98fa8ea82b9efd14ba79f48c70a0d
Caio Cichetti <caio_cichetti@outlook.com>
2020-07-17 15:09:28 -0300

    refatorando js

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,9 @@
 function verificaDelete(Id) {
-    let id = Id;
+  let id = Id;
 
-    if (confirm("Deseja excluir este projeto? ")) {
-      location.href=`delete.php?id=${id}`;
-    }
+  if (confirm("Deseja excluir este projeto? ")) {
+    location.href = `delete.php?id=${id}`;
+  }
 }
 
 function enviarEmail(email, nome, projeto, nomeFreela, profFreela) {
@@ -13,7 +13,7 @@ function enviarEmail(email, nome, projeto, nomeFreela, profFreela) {
   let Nomefreelancer = nomeFreela;
   let ProfFreelancer = profFreela;
 
-  if(confirm(`Você deseja enviar um e-mail para ${Nome}`)) {
-    location.href = `mailto:${Email}?subject=Interesse no projeto ${Projeto}&body=Olá ${Nome}! Meu nome é ${Nomefreelancer} Sou ${ProfFreelancer} e vi seu projeto ${Projeto} na plataforma Find Jobs e eu tenho interesse em ajudar.`
+  if (confirm(`Você deseja enviar um e-mail para ${Nome}`)) {
+    location.href = `mailto:${Email}?subject=Interesse no projeto ${Projeto}&body=Olá ${Nome}! Meu nome é ${Nomefreelancer} Sou ${ProfFreelancer} e vi seu projeto ${Projeto} na plataforma Find Jobs e eu tenho interesse em ajudar.`;
   }
-} 
+}
