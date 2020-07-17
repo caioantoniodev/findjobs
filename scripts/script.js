613660a7d076b609853e30298d4a13d403cb7815
Caio Cichetti <caio_cichetti@outlook.com>
2020-07-17 11:57:18 -0300

    arrumando erro

--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,15 @@ function verificaDelete(Id) {
       location.href=`delete.php?id=${id}`;
     }
 }
+
+function enviarEmail(email, nome, projeto, nomeFreela, profFreela) {
+  let Email = email;
+  let Nome = nome;
+  let Projeto = projeto;
+  let nomefreela = nomeFreela;
+  let profFreela = profFreela;
+
+  if(confirm(`Você deseja enviar um e-mail para ${Nome}`)) {
+    location.href = `mailto:${Email}?subject=Interesse no projeto ${Projeto}&body=Olá ${Nome}! Meu nome é ${nomefreela} Sou desenvolvedor back-end e vi seu projeto ${Projeto} na plataforma Find Jobs e eu tenho interesse em ajudar.`
+  }
+} 
