e0987397acaf52d23272a84d86e621179b3c457d
Caio Cichetti <caio_cichetti@outlook.com>
2020-07-17 12:02:37 -0300

    Update loading.js

--- scripts/loading.js
+++ scripts/loading.js
@@ -4,6 +4,6 @@ var i = setInterval(function () {
     // este codigo mostra o delay do termino de mostrar uma div:
     //usei este codigo com um fake loading in css para sumir apos alguns segundos.
     document.getElementById("content").style.display = "none";
-    document.getElementById("conteudo").style.display = "block";
+    // document.getElementById("conteudo").style.display = "block";
     document.getElementById("enviar").style.display = "block";
 }, 1500);
