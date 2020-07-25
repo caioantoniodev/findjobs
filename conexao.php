1c17232cbcb6092ec432b180f3ee84ddb64758d5
Caio Cichetti <caio_cichetti@outlook.com>
2020-07-25 00:31:08 -0300

    Delete conexao.php

--- conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-  // Criando a conexão com o servidor do MySQL Windows
-  $conexao = mysqli_connect('localhost', 'root', '', 'findjobs') 
-    or die ('Não foi possível acessar o BD');
-  // Criando a conexão com o servidor do MySQL Linux Docker
-  // $conexao = mysqli_connect('172.17.0.2', 'root', '', 'findjobs') 
-  //   or die ('Não foi possível acessar o BD');
-?>
