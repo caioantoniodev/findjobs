09466c3d27c092dc30b175c706a7b406760a485f
Caio Cichetti <caio_cichetti@outlook.com>
2020-07-24 22:52:23 -0300

    Update processa_avaliacao.php

--- processa_avaliacao.php
+++ processa_avaliacao.php
@@ -4,25 +4,25 @@
 
   if(empty($_POST['nome_completo']) || empty($_POST['profissao']) || empty($_POST['opniao']) || empty($_POST['estrela'])) {
 		$_SESSION['mal_sucedido'] = TRUE;
-    header('Location: avaliacoes.php');
+    header('Location: index.php');
   } else {
 		$opn = $_POST['opniao'];
 
 		$estrela = $_POST['estrela'];
-	
+
 		$cpf = $_SESSION['cpf'];
-	
+
 		// Salvar no banco de dados
 		$sql = "INSERT INTO reclamacoes (opniao, usuario_cpf, estrelas) VALUES ('$opn', '$cpf', '$estrela');";
-		
+
 		if(mysqli_query($conexao, $sql) === TRUE) {
 			$_SESSION['bem_sucedido'] = true;
-		} 
-	
+		}
+
 		mysqli_close($conexao);
-	
-		header("Location: avaliacoes.php");
-	
+
+		header("Location: index.php");
+
 		exit();
 	}
 ?>
