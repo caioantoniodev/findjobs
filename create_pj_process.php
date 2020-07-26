ef195f111e5c228838be7c35d93d64e4d5398118
Caio Cichetti <caio_cichetti@outlook.com>
2020-07-26 14:57:03 -0300

    Update create_pj_process.php

--- create_pj_process.php
+++ create_pj_process.php
@@ -1,28 +1,28 @@
 <?php
-	session_start();
-  include_once("connection.php");
+session_start();
+include_once("connection.php");
 
-  if(empty($_POST['nome_projeto']) || empty($_POST['lang']) || empty($_POST['descricao']) || empty($_POST['valor'])) {
-    $_SESSION['mal_sucedido'] = TRUE;
-    header('Location: create_pj_process.php');
-  }  else {
+if (empty($_POST['nome_projeto']) || empty($_POST['lang']) || empty($_POST['descricao']) || empty($_POST['valor'])) {
+  $_SESSION['mal_sucedido'] = TRUE;
+  header('Location: create_pj_process.php');
+} else {
 
-    $nomeProjeto = trim($_POST['nome_projeto']);
-    $linguagem = trim($_POST['lang']);
-    $descricao = trim($_POST['descricao']);
-    $valorProjeto = trim($_POST['valor']);
-    $cpfCliente = trim($_POST['cpfCliente']);
-    $dataEntrega = trim($_POST['dataEntrega']);
-    $pagamento = trim($_POST['fPagamento']);
+  $nomeProjeto = trim($_POST['nome_projeto']);
+  $linguagem = trim($_POST['lang']);
+  $descricao = trim($_POST['descricao']);
+  $valorProjeto = trim($_POST['valor']);
+  $cpfCliente = trim($_POST['cpfCliente']);
+  $dataEntrega = trim($_POST['dataEntrega']);
+  $pagamento = trim($_POST['fPagamento']);
 
-    // Pego a imagem e movo para a pasta uploads e pego o caminho da imagem do usuario
-    $destino = 'uploads/' .  $_FILES['imagem']['name'];
-    $imagem = $_FILES['imagem']['tmp_name'];
-    $caminho =  move_uploaded_file($imagem, $destino);
+  // Pego a imagem e movo para a pasta uploads e pego o caminho da imagem do usuario
+  $destino = 'uploads/' .  $_FILES['imagem']['name'];
+  $imagem = $_FILES['imagem']['tmp_name'];
+  $caminho =  move_uploaded_file($imagem, $destino);
 
 
-		// Salvar no banco de dados
-    $sql = "INSERT INTO projetos (
+  // Salvar no banco de dados
+  $sql = "INSERT INTO projetos (
       titulo,
       descricao,
       salario,
@@ -44,11 +44,11 @@
       '$pagamento'
     )";
 
-    if(mysqli_query($conexao, $sql) === TRUE) {
-			$_SESSION['criar_sucedido'] = true;
-		}
-
-		mysqli_close($conexao);
-
-		header("Location: profile.php");
+  if (mysqli_query($conexao, $sql) === TRUE) {
+    $_SESSION['criar_sucedido'] = true;
   }
+
+  mysqli_close($conexao);
+
+  header("Location: profile.php");
+}
