0fd68a3a6567c1d12cc8a7d84e06525d03343c3a
Caio Cichetti <caio_cichetti@outlook.com>
2020-07-27 22:14:28 -0300

    Style Guides of PHP

--- profile_update.php
+++ profile_update.php
@@ -1,17 +1,17 @@
 <?php
 
-  session_start();
-  include("connection.php");
+session_start();
+include("connection.php");
 
-  $cpf = $_SESSION['cpf'];
+$cpf = $_SESSION['cpf'];
 
-  $nome = $_POST['nome'];
-  $profissao = $_POST['profissao'];
-  $sobre = $_POST['sobre'];
-  $tel = $_POST['tel'];
-  $exp = $_POST['experiencia'];
+$nome = $_POST['nome'];
+$profissao = $_POST['profissao'];
+$sobre = $_POST['sobre'];
+$tel = $_POST['tel'];
+$exp = $_POST['experiencia'];
 
-  $sql = "UPDATE usuarios
+$sql = "UPDATE usuarios
             SET usuarios.nome='$nome',
                 usuarios.profissao='$profissao',
                 usuarios.sobre='$sobre',
@@ -19,24 +19,24 @@
                 usuarios.experiencia='$exp'
             WHERE usuarios.cpf = '$cpf'";
 
-  $github = $_POST['github'];
-  $twiter = $_POST['twitter'];
-  $instagram = $_POST['instagram'];
+$github = $_POST['github'];
+$twiter = $_POST['twitter'];
+$instagram = $_POST['instagram'];
 
-  // Pego o cpf passado pelo usuario e verifico se já tem as redes socias cadastradas
-  // com esse cpf
-  $query = "SELECT COUNT(*) AS total FROM redes_sociais WHERE usuarios_cpf = '$cpf'";
-  $resultado = mysqli_query($conexao, $query);
-  $row = mysqli_fetch_assoc($resultado);
+// Pego o cpf passado pelo usuario e verifico se já tem as redes socias cadastradas
+// com esse cpf
+$query = "SELECT COUNT(*) AS total FROM redes_sociais WHERE usuarios_cpf = '$cpf'";
+$resultado = mysqli_query($conexao, $query);
+$row = mysqli_fetch_assoc($resultado);
 
-  if ($row['total'] == 1) {
-    $sql2 = "UPDATE 	redes_sociais
+if ($row['total'] == 1) {
+  $sql2 = "UPDATE 	redes_sociais
           SET instagram = '$instagram',
           twitter = '$twiter',
           github = '$github'
           WHERE usuarios_cpf = $cpf";
-  } else {
-    $sql2 = "INSERT INTO redes_sociais (
+} else {
+  $sql2 = "INSERT INTO redes_sociais (
           github,
           instagram,
           twitter,
@@ -47,35 +47,35 @@
           '$instagram',
           '$cpf'
         )";
-  }
+}
 
-  if (empty($_FILES['img']['name'])) {
-    $consulta = "SELECT usuarios.avatarurl FROM usuarios WHERE usuarios.cpf = $cpf";
+if (empty($_FILES['img']['name'])) {
+  $consulta = "SELECT usuarios.avatarurl FROM usuarios WHERE usuarios.cpf = $cpf";
 
-    // recebo o resutado da querie na variavel $resultado
-    $resultado = mysqli_query($conexao, $consulta);
+  // recebo o resutado da querie na variavel $resultado
+  $resultado = mysqli_query($conexao, $consulta);
 
-    $img = mysqli_fetch_assoc($resultado);
+  $img = mysqli_fetch_assoc($resultado);
 
-    $url = $img['avatarurl'];
+  $url = $img['avatarurl'];
 
-    $cons = "UPDATE usuarios
+  $cons = "UPDATE usuarios
           SET avatarurl = '$url'
           WHERE usuarios.cpf = $cpf";
-  } else {
-    // Pego a imagem e movo para a pasta uploads e pego o caminho da imagem do usuario
-    $destino = 'uploads/' .  $_FILES['img']['name'];
-    $imagem = $_FILES['img']['tmp_name'];
-    $caminho =  move_uploaded_file($imagem, $destino);
+} else {
+  // Pego a imagem e movo para a pasta uploads e pego o caminho da imagem do usuario
+  $destino = 'uploads/' .  $_FILES['img']['name'];
+  $imagem = $_FILES['img']['tmp_name'];
+  $caminho =  move_uploaded_file($imagem, $destino);
 
-    $cons = "UPDATE usuarios SET avatarurl = '$destino' WHERE usuarios.cpf = $cpf";
-  }
+  $cons = "UPDATE usuarios SET avatarurl = '$destino' WHERE usuarios.cpf = $cpf";
+}
 
-  if (mysqli_query($conexao, $sql) && mysqli_query($conexao, $sql2) && mysqli_query($conexao, $cons)) {
-    $_SESSION['update_sucedido'] = TRUE;
-  } else {
-    $_SESSION['update_error'] = TRUE;
-  }
+if (mysqli_query($conexao, $sql) && mysqli_query($conexao, $sql2) && mysqli_query($conexao, $cons)) {
+  $_SESSION['update_sucedido'] = TRUE;
+} else {
+  $_SESSION['update_error'] = TRUE;
+}
 
-  header('Location: profile.php');
-  mysqli_close($conexao);
+header('Location: profile.php');
+mysqli_close($conexao);
