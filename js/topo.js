a3bd830e40b2cffc69a12eb420e370f154c1b291
Caio Cichetti <caio_cichetti@outlook.com>
2020-07-27 22:14:36 -0300

    Style Guides of PHP

--- js/topo.js
+++ js/topo.js
@@ -1,17 +1,16 @@
-window.onscroll = function(){
-    scroll();
+window.onscroll = function () {
+  scroll();
 };
 
-function scroll(){
-    let btn = document.getElementById("btnTop");
-    if(document.documentElement.scrollTop > 50){
-        btn.style.display = "block"
-    }
-    else{
-        btn.style.display = "none"
-    }
+function scroll() {
+  let btn = document.getElementById('btnTop');
+  if (document.documentElement.scrollTop > 50) {
+    btn.style.display = 'block';
+  } else {
+    btn.style.display = 'none';
+  }
 }
 
-function backToTop(){
-    document.documentElement.scrollTop = 0;
+function backToTop() {
+  document.documentElement.scrollTop = 0;
 }
