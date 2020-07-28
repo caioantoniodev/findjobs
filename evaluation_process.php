4cf812128a6e039c07522ce37f266026b371c2d5
Caio Cichetti <caio_cichetti@outlook.com>
2020-07-27 22:14:43 -0300

    Style Guides of PHP

--- evaluation_process.php
+++ evaluation_process.php
@@ -1,27 +1,27 @@
 <?php
-	session_start();
-	include_once("connection.php");
+session_start();
+include_once("connection.php");
 
-  if(empty($_POST['nome_completo']) || empty($_POST['profissao']) || empty($_POST['opniao']) || empty($_POST['estrela'])) {
-		$_SESSION['mal_sucedido'] = TRUE;
-    header('Location: index.php');
-  } else {
-		$opn = $_POST['opniao'];
+if (empty($_POST['nome_completo']) || empty($_POST['profissao']) || empty($_POST['opniao']) || empty($_POST['estrela'])) {
+  $_SESSION['mal_sucedido'] = TRUE;
+  header('Location: index.php');
+} else {
+  $opn = $_POST['opniao'];
 
-		$estrela = $_POST['estrela'];
+  $estrela = $_POST['estrela'];
 
-		$cpf = $_SESSION['cpf'];
+  $cpf = $_SESSION['cpf'];
 
-		// Salvar no banco de dados
-		$sql = "INSERT INTO reclamacoes (opniao, usuario_cpf, estrelas) VALUES ('$opn', '$cpf', '$estrela')";
+  // Salvar no banco de dados
+  $sql = "INSERT INTO reclamacoes (opniao, usuario_cpf, estrelas) VALUES ('$opn', '$cpf', '$estrela')";
 
-		if(mysqli_query($conexao, $sql) === TRUE) {
-			$_SESSION['avbem_sucedido'] = true;
-		}
+  if (mysqli_query($conexao, $sql) === TRUE) {
+    $_SESSION['avbem_sucedido'] = true;
+  }
 
-		mysqli_close($conexao);
+  mysqli_close($conexao);
 
-		header("Location: index.php");
+  header("Location: index.php");
 
-		exit();
-	}
+  exit();
+}
