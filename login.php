4f39ad4559c9cc3556354282da07e77bb223458c
Caio Cichetti <caio_cichetti@outlook.com>
2020-08-15 10:24:39 -0300

    translate login page

--- login.php
+++ login.php
@@ -29,7 +29,7 @@ session_start();
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ml-auto text-center">
             <li class="nav-item active">
-              <a class="nav-link text-white" href="index.php">Tela Inicial</a>
+              <a class="nav-link text-white" href="index.php">Home</a>
             </li>
             <li class="nav-item">
               <a class="nav-link text-white" href="register.php">Cadastrar</a>
